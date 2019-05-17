a2fb14cb3e30f56a1409a0140d975a2ffcaca487
Laura Badgley <l.badgleyy@gmail.com>
2019-05-16 17:48:24 -0700

    quotes built

---
+++ src/components/quotes/Quotes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Quote from './Quote';
+import styles from './Quotes.css';
+
+export default function Quotes({ quotes }) {
+  const quoteList = quotes.map((quote, i) => (
+    <li key={i}>
+      <Quote quote={quote} />
+    </li>
+  ));
+
+  return (
+    <ul className={styles.Quotes}>
+      {quoteList}
+    </ul>
+  );
+}
+
+Quotes.propTypes = {
+  quotes: PropTypes.array.isRequired
+};
