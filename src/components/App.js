2ccc4dde7221e22a237ee417044d35bd8fa7fce0
Laura Badgley <l.badgleyy@gmail.com>
2019-05-16 17:15:25 -0700

    app snapshot test passing

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,16 @@
-import React from 'react';
-import BestQuotes from '.'
+import React, { useState } from 'react';
+import BestQuotes from '../containers/quotes/BestQuotes';
+import CharacterQuotes from '../containers/quotes/CharacterQuotes';
+
+export default function App() {
+  const [quotes, updatesQuotes] = useState('top');
+
+  return (
+    <>
+      <button onClick={() => updatesQuotes('top')}>Best Quotes</button>
+      <button onClick={() => updatesQuotes('character')}>Character Quotes</button>
+      {quotes === 'top' && <BestQuotes />}
+      {quotes === 'character' && <CharacterQuotes />}
+    </>
+  );
+}
