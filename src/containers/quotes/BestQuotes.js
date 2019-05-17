24a84ca43fbeb55dfcb7810c386d7313723f3610
Laura Badgley <l.badgleyy@gmail.com>
2019-05-16 17:47:52 -0700

    loading built and container for quotes built

---
+++ src/containers/quotes/BestQuotes.js
@@ -0,0 +1,43 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Quotes from '../../containers/quotes/CharacterQuotes';
+import Loading from '../../components/Loading';
+import { getQuotes } from '../../';
+
+export default class BestQuotes extends PureComponent {
+  static propTypes = {
+    count: PropTypes.number
+  }
+
+  static defaultProps = {
+    count: 10
+  }
+
+  state = {
+    quote: [],
+    loading: true
+  }
+
+  fetchQuotes = () => {
+    this.setState({ loading: true });
+    getQuotes(this.props.count)
+      .then(quotes => this.setState({ quotes, loading: false }));
+  }
+
+  componentDidMount() {
+    this.fetchQuotes();
+  }
+
+  componentDidUpdate(prevProps) {
+    if(prevProps.count !== this.props.count) {
+      this.fetchQuotes();
+    }
+  }
+
+  render() {
+    const { quotes, loading } = this.state;
+    if(loading) return <Loading />;
+
+    return <Quotes quotes={quotes} />;
+  }
+}
