9e6c2af95d35acdfe0393bff9749c78b38a2654b
Laura Badgley <l.badgleyy@gmail.com>
2019-05-19 13:42:29 -0700

    quotes written and passing

---
+++ src/components/quotes/Quotes.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Quote from './Quote';
+
+describe('quote test', () => {
+  it('renders a quote', () => {
+    const quote = {
+      character: 'Leela',
+      quote: 'Quote',
+      image: '/image'
+    };
+
+    const wrapper = shallow(<Quote quote={quote} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+
