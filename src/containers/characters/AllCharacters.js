f73c2f4ba50b4206bb09a0a350ac8fc7a2ebc975
Laura Badgley <l.badgleyy@gmail.com>
2019-05-19 22:01:43 -0700

    AllCharacters written and styled

--- src/containers/characters/AllCharacters.js
+++ src/containers/characters/AllCharacters.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import Characters from '../../components/characters/Characters';
 import Loading from '../../components/Loading';
+import { getCharacters } from '../../services/rickAndMortyApi';
 
 export default class AllCharacters extends PureComponent {
   state = {
@@ -8,14 +10,22 @@ export default class AllCharacters extends PureComponent {
     characters: [],
     loading: true
   }
+  
+  fetchCharacters = () => {
+    this.setState({ loading: true });
+    getCharacters()
+      .then(({ characters }) => this.setState({ characters, loading: false }));
+  }
+
+  componentDidMount() {
+    this.fetchCharacters();
+  }
 
   render() {
-    const {
-      currentPage,
-      totalPages,
-      characters,
-      loading
-    } = this.state;
+    const { characters, loading } = this.state;
+
+    if(loading) return <Loading />;
+
+    return <Characters characters={characters} />;
   }
-  if(loading) return <Loading />;
 }
